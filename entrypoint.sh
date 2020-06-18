069b96ab007e28818108bb243c74edb420718d63
Ayaz43 <65758480+Ayaz43@users.noreply.github.com>
2020-06-18 15:46:28 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,6 @@ RUN apk add --no-cache curl tar openssl sudo bash jq
 RUN apk --update --no-cache add postgresql-client postgresql
 
 ENV AZURE_CLI_VERSION 2.0.72
-RUN apk add py-pip && \
-    apk add --virtual=build gcc libffi-dev musl-dev openssl-dev python-dev make
+RUN apk add py-pip && apk add --virtual=build gcc libffi-dev musl-dev openssl-dev python-dev make
 
 RUN pip --no-cache-dir install azure-cli==${AZURE_CLI_VERSION}
