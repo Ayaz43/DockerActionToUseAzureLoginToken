cb404eb652b4269e19ce9c3479759c4bf012a33e
Ayaz43 <65758480+Ayaz43@users.noreply.github.com>
2020-06-18 12:12:37 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,7 @@ echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
 ls /github/workspace/
+echo "list $GITHUB_WORKSPACE/.github/workflows/.azure"
 ls -a $GITHUB_WORKSPACE/.github/workflows/.azure
+echo "list /home/runner/work/sampleWorkflow/sampleWorkflow/.azure"
+ls -a /home/runner/work/sampleWorkflow/sampleWorkflow/.azure
