d7f4c0a49a9b01d7c7bf537244ef218e39c5a3f4
Ayaz43 <65758480+Ayaz43@users.noreply.github.com>
2020-06-20 20:27:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,10 @@
 echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
-ls /github/workspace/
 
-echo "list $GITHUB_WORKSPACE/"
-ls -a $GITHUB_WORKSPACE/
-
-
+echo "******************************************************"
+echo "list $GITHUB_WORKSPACE/.github/workflows/.azure"
+ls -a $GITHUB_WORKSPACE/.github/workflows/.azure
 
 echo "Azure version"
 az --version
-
-echo "Azure account"
-az account show
-
