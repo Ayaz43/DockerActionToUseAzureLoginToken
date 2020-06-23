c5bd3f637b3d77421f2c6456e74bd1bd7a26b8df
Ayaz43 <65758480+Ayaz43@users.noreply.github.com>
2020-06-23 18:22:37 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -l
-echo " HOME "
-ls -a $HOME
-echo " GITHUB_WORKFLOW "
-ls -a $GITHUB_WORKFLOW
-echo " GITHUB_WORKSPACE "
-ls -a $GITHUB_WORKSPACE
+
 cp -R $GITHUB_WORKSPACE/.azure /github/home/.azure
+
 az account show
